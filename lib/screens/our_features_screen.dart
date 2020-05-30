10be7e335df773f64a658a54645d46d0a4cc1d68
Aditya Gupta <aditya.gupta181099@gmail.com>
2020-05-31 01:16:41 +0530

    testing

---
+++ lib/screens/our_features_screen.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import '../widgets/app_drawer.dart';
+
+class OurFeatures extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    // static String routeName = '/'
+    return Scaffold(
+      backgroundColor: Hexcolor('#E7F3FF'),
+      key: _scaffoldKey,
+      drawer: AppDrawer(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              width: double.infinity,
+              // height: mediaQuery.size.height * 0.28,
+              decoration: BoxDecoration(
+                color: Hexcolor('#E7F3FF'),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/our feature page bg.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: IconButton(
+                      icon: Icon(Icons.menu),
+                      onPressed: () => _scaffoldKey.currentState.openDrawer(),
+                    ),
+                  ),
+                  Container(
+                    height: 150,
+                    padding: EdgeInsets.only(top: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          // color: Colors.red,
+
+                          margin: EdgeInsets.only(
+                            left: mediaQuery.size.height * 0.02,
+                          ),
+                          // padding: EdgeInsets.all(30),
+
+                          child: Text(
+                            'Our',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 45,
+                              letterSpacing: 2,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          // color: Colors.blue,
+                          margin: EdgeInsets.only(
+                            left: mediaQuery.size.height * 0.02,
+                          ),
+                          // padding: EdgeInsets.only(top: mediaQuery.size.height * 0.078,),
+
+                          child: Text(
+                            'Features',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              fontSize: 45,
+                              height: 1,
+                              letterSpacing: 2,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: mediaQuery.size.height * 0.05,
+                bottom: mediaQuery.size.height * 0.095,
+              ),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    // margin: EdgeInsets.only(top: mediaQuery.size.height * 0.04),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(32),
+                        bottomRight: Radius.circular(32),
+                      ),
+                      child: Container(
+                        padding: EdgeInsets.only(left: 20),
+                        alignment: Alignment.centerLeft,
+                        height: 50,
+                        width: 120,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [Hexcolor('#79B6FC'), Colors.white],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            stops: [0.5, 1.8],
+                          ),
+                        ),
+                        child: Text(
+                          'Features',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Card(
+                        color: Hexcolor('#E7F3FF'),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              child: Text(
+                                'We',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 35
+                                ), 
+                              ), 
+                            ),
+                            Container(
+                              child: Text(
+                                'Create',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 35
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            // Container(
+            //   height: mediaQuery.size.height * 0.3,
+            //   color: Colors.amber,
+            // ),
+          ],
+        )),
+      ),
+    );
+  }
+}
