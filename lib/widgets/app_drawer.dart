1df96aed98813ed66b28adb6cdef2205577fbfec
Aditya Gupta <aditya.gupta181099@gmail.com>
2020-05-31 02:24:17 +0530

    home screen completed

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,144 +1,217 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(
-              top: 43,
-              left: 0
-            ),
-            padding: EdgeInsets.zero,
-            alignment: Alignment.centerLeft,
-            child: IconButton(
-              alignment: Alignment.topLeft,
-              padding: EdgeInsets.only(left: 20),
-              icon: Icon(
-                Icons.clear,
-                size: 20,
+    final mediaQuery = MediaQuery.of(context);
+
+    return SafeArea(
+      child: Container(
+        width: mediaQuery.size.width * 0.75,
+        
+        child: Drawer(
+          elevation: 5,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/drawer bg.png'),
+                  
+                ),
+                color: Hexcolor('#E3F0FC'),
               ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
-          SizedBox(height:45), 
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text(
-              'Home',
-              style: TextStyle(
-                fontSize: 16,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.clear,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed('/');
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.group_work,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Our Features',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.extension,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Services',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Hexcolor('#2D2871'),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.dns,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'What we do',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Hexcolor('#2D2871'),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.group,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Our Team',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Hexcolor('#2D2871'),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.queue,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Join Us',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.live_help,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'Contact Us',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.info,
+                      color: Hexcolor('#2D2871'),
+                    ),
+                    title: Text(
+                      'About Us',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                    },
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: mediaQuery.size.height * 0.30),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.web,
+                        color: Hexcolor('#2D2871'),
+                      ),
+                      title: Text(
+                        'Website',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      trailing: Icon(
+                        Icons.launch,
+                        color: Hexcolor('#2D2871'),
+                      ),
+                      onTap: () {
+                        // Navigator.of(context).pushReplacementNamed(Screen.routeName);
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed('/');
-            },
           ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.payment),
-            title: Text(
-              'Our Features',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'Services',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-            trailing: IconButton(
-              icon: Icon(Icons.keyboard_arrow_down),
-              onPressed: () {},
-            ),
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'What we do',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-            trailing: IconButton(
-              icon: Icon(Icons.keyboard_arrow_down),
-              onPressed: () {},
-            ),
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'Our Team',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-            trailing: IconButton(
-              icon: Icon(Icons.keyboard_arrow_down),
-              onPressed: () {},
-            ),
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'Join Us',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'Contact Us',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-          ),
-          SizedBox(height: 10),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'About Us',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-          ),
-          SizedBox(height: 186),
-          ListTile(
-            leading: Icon(Icons.edit),
-            title: Text(
-              'Website',
-              style: TextStyle(fontSize: 16),
-            ),
-            onTap: () {
-              // Navigator.of(context).pushReplacementNamed(Screen.routeName);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
